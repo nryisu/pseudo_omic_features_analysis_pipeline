ecd92a1d592902a05431cc9cb8f22f329f7332c6
nryisu <892272536@qq.com>
2024-11-11 13:58:15 +0800

    Update deg_preprocess.sh

--- Figure4/deg_preprocess.sh
+++ Figure4/deg_preprocess.sh
@@ -23,5 +23,12 @@ bamCoverage -bs 10 -b esc.bam -o esc.bw
 ####################################################################################
 
 ###single cell RNA 
-#Please refer to the "scRNA_DEGs_pipeline.md" documentation.
+## scRNA-seq datasets
+The datasets from [Kumar et al.](https://www.nature.com/articles/nature13920). The raw data can be download from [GSE60749](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE60749). We downloaded the individual nestin-positive neural precursor cells and v6.5 mESCs cultured in serum+LIF media for comparison.
+## Annotation
+The annotation folder contains detailed information for analysis.
+## Alignment
+The alignment folder contains the commands for obtaining the count matrix from the raw data(fastq file format).
+## Downstream 
+The downstream folder contains the commands for downstream analysis from the count matrix.
 
